6807a8b78b059d00567d00c94b304fb95ff295b2
Qian <qiding@redhat.com>
2025-05-22 18:11:17 +0800

    test

--- .github/actions/log_to_splunk/main.py
+++ .github/actions/log_to_splunk/main.py
@@ -76,78 +76,78 @@ def main():
     print("event:")
     print(event)
 
-    x=requests.post(SPLUNK_HEC_URL, data=event, headers=headers)
+    response=requests.post(SPLUNK_HEC_URL, data=event, headers=headers)
 
 
-    url = "{url}/repos/{repo}/actions/runs/{run_id}/logs".format(url=GITHUB_API_URL,repo=GITHUB_REPOSITORY,run_id=GITHUB_WORKFLOWID)
-    print(url)
+    # url = "{url}/repos/{repo}/actions/runs/{run_id}/logs".format(url=GITHUB_API_URL,repo=GITHUB_REPOSITORY,run_id=GITHUB_WORKFLOWID)
+    # print(url)
 
-    try:
-        x = requests.get(url, stream=True, auth=('token',GITHUB_TOKEN))
+    # try:
+    #     x = requests.get(url, stream=True, auth=('token',GITHUB_TOKEN))
 
-    except requests.exceptions.HTTPError as errh:
-        output = "GITHUB API Http Error:" + str(errh)
-        print(f"Error: {output}")
-        print(f"::set-output name=result::{output}")
-        return
-    except requests.exceptions.ConnectionError as errc:
-        output = "GITHUB API Error Connecting:" + str(errc)
-        print(f"Error: {output}")
-        print(f"::set-output name=result::{output}")
-        return
-    except requests.exceptions.Timeout as errt:
-        output = "Timeout Error:" + str(errt)
-        print(f"Error: {output}")
-        print(f"::set-output name=result::{output}")
-        return
-    except requests.exceptions.RequestException as err:
-        output = "GITHUB API Non catched error conecting:" + str(err)
-        print(f"Error: {output}")
-        print(f"::set-output name=result::{output}")
-        return
+    # except requests.exceptions.HTTPError as errh:
+    #     output = "GITHUB API Http Error:" + str(errh)
+    #     print(f"Error: {output}")
+    #     print(f"::set-output name=result::{output}")
+    #     return
+    # except requests.exceptions.ConnectionError as errc:
+    #     output = "GITHUB API Error Connecting:" + str(errc)
+    #     print(f"Error: {output}")
+    #     print(f"::set-output name=result::{output}")
+    #     return
+    # except requests.exceptions.Timeout as errt:
+    #     output = "Timeout Error:" + str(errt)
+    #     print(f"Error: {output}")
+    #     print(f"::set-output name=result::{output}")
+    #     return
+    # except requests.exceptions.RequestException as err:
+    #     output = "GITHUB API Non catched error conecting:" + str(err)
+    #     print(f"Error: {output}")
+    #     print(f"::set-output name=result::{output}")
+    #     return
 
-    z = zipfile.ZipFile(io.BytesIO(x.content))
-    z.extractall('/app')
+    # z = zipfile.ZipFile(io.BytesIO(x.content))
+    # z.extractall('/app')
 
-    timestamp = batch = count = 0
+    # timestamp = batch = count = 0
 
-    for name in glob.glob('/app/*.txt'):
-        logfile = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), name.replace('./','')),'r')
-        Lines = logfile.readlines()
-        for line in Lines:
+    # for name in glob.glob('/app/*.txt'):
+    #     logfile = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), name.replace('./','')),'r')
+    #     Lines = logfile.readlines()
+    #     for line in Lines:
 
-            if line:
-                count+=1
-                if timestamp:
-                    t2=timestamp
-                timestamp = re.search("\d{4}-\d{2}-\d{2}T\d+:\d+:\d+.\d+Z",line.strip())
+    #         if line:
+    #             count+=1
+    #             if timestamp:
+    #                 t2=timestamp
+    #             timestamp = re.search("\d{4}-\d{2}-\d{2}T\d+:\d+:\d+.\d+Z",line.strip())
 
-                if timestamp:
-                    timestamp = re.sub("\dZ","",timestamp.group())
-                    timestamp = datetime.strptime(timestamp,"%Y-%m-%dT%H:%M:%S.%f")
-                    timestamp = (timestamp - datetime(1970,1,1)).total_seconds()
-                else:
-                    timestamp=t2
+    #             if timestamp:
+    #                 timestamp = re.sub("\dZ","",timestamp.group())
+    #                 timestamp = datetime.strptime(timestamp,"%Y-%m-%dT%H:%M:%S.%f")
+    #                 timestamp = (timestamp - datetime(1970,1,1)).total_seconds()
+    #             else:
+    #                 timestamp=t2
 
-                x = re.sub("\d{4}-\d{2}-\d{2}T\d+:\d+:\d+.\d+Z","",line.strip())
-                x=x.strip()
-                job_name=re.search("\/\d+\_(?P<job>.*)\.txt",name)
-                job_name=job_name.group('job')
+    #             x = re.sub("\d{4}-\d{2}-\d{2}T\d+:\d+:\d+.\d+Z","",line.strip())
+    #             x=x.strip()
+    #             job_name=re.search("\/\d+\_(?P<job>.*)\.txt",name)
+    #             job_name=job_name.group('job')
 
-                fields = {'lineNumber':count,'workflowID':GITHUB_WORKFLOWID,'job':job_name}
-                if x:
-                    batch+=1
-                    event={'event':x,'sourcetype':SPLUNK_SOURCETYPE,'source':SPLUNK_SOURCE,'host':host,'time':timestamp,'fields':fields}
-                    eventBatch=eventBatch+json.dumps(event)
-                else:
-                    print("skipped line "+str(count))
+    #             fields = {'lineNumber':count,'workflowID':GITHUB_WORKFLOWID,'job':job_name}
+    #             if x:
+    #                 batch+=1
+    #                 event={'event':x,'sourcetype':SPLUNK_SOURCETYPE,'source':SPLUNK_SOURCE,'host':host,'time':timestamp,'fields':fields}
+    #                 eventBatch=eventBatch+json.dumps(event)
+    #             else:
+    #                 print("skipped line "+str(count))
 
-                if batch>=1000:
-                    batch=0
-                    x=requests.post(SPLUNK_HEC_URL, data=eventBatch, headers=headers)
-                    eventBatch=""
+    #             if batch>=1000:
+    #                 batch=0
+    #                 x=requests.post(SPLUNK_HEC_URL, data=eventBatch, headers=headers)
+    #                 eventBatch=""
 
-    response=requests.post(SPLUNK_HEC_URL, data=eventBatch, headers=headers)
+    # response=requests.post(SPLUNK_HEC_URL, data=eventBatch, headers=headers)
     
     if response.ok:
         print("Logs forwarding successfully!")
